0e9463d01c50ce52129d425f8562da00d56e54f3
TakeruMizoguchi <mizotake123@gmail.com>
2016-10-23 23:35:42 +0900

    Change Layout

--- MemoTech/Pages/MemoTechPage.xaml.cs
+++ MemoTech/Pages/MemoTechPage.xaml.cs
@@ -6,22 +6,35 @@ namespace MemoTech
 
 	public partial class MemoTechPage : ContentPage
 	{
+		private State buttonState = State.Start;
+		private enum State
+		{
+			Start = 0,
+			Share = 1
+		}
+		private string[] stateTitle = { "Start", "Share" };
+
 		public MemoTechPage()
 		{
 			InitializeComponent();
 
 			Title = "TitlePage";
 
-			var start = this.FindByName<Button>("NextShare");
-			start.Clicked += async (sender, arg) =>
+			var mainButton = this.FindByName<Button>("MainButton");
+			mainButton.Clicked += async (sender, arg) =>
 			{
-				await Navigation.PushAsync(new SharePage());
-			};
-
-			var memory = this.FindByName<Button>("NextMemory");
-			memory.Clicked += async (sender, arg) =>
-			{
-				await Navigation.PushAsync(new MemoryPage());
+				switch (buttonState)
+				{
+					case State.Start:
+						buttonState = State.Share;
+						break;
+					case State.Share:
+						buttonState = State.Start;
+						//仮処理で0
+						await Navigation.PushAsync(new AlbumPage(0));
+						break;
+				}
+				mainButton.Text = stateTitle[(int)buttonState];
 			};
 		}
 	}
